0c6a8918c67cbad31a661df4f05b483fd00d8ceb
3sdd <56486103+3sdd@users.noreply.github.com>
2022-11-17 23:23:48 +0900

    mswでモックapiを試した

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,14 @@ import router from "./router";
 
 import "./assets/main.css";
 
+if (import.meta.env.DEV) {
+  const { worker } = await import("./mocks/browser");
+  worker.start({
+    // デフォルト設定だとwarningがたくさん出るため
+    onUnhandledRequest: "bypass",
+  });
+}
+
 const app = createApp(App);
 
 app.use(router);
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,32 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import axios from "axios";
+import { ref } from "vue";
+
+type User = {
+  username: string;
+};
+
+const user = ref<User | null>(null);
+
+async function onLoginClick() {
+  try {
+    await axios.post("/login");
+  } catch (e) {
+    alert("ログインエラー");
+    return;
+  }
+
+  const { data: u } = await axios.get("/user");
+  user.value = u;
+}
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <button @click="onLoginClick">login</button>
+    <div>
+      {{ user === null ? "未ログイン" : "ログイン済み" }}
+    </div>
+    <div v-if="user">ユーザー名:{{ user.username }}</div>
   </main>
 </template>
